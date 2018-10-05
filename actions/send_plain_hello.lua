d8a6769800997c7390896cbe16d47e430e1340c9
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-10-06 00:53:37 +0200

    actions in new yaml format

--- actions/send_plain_hello.lua
+++ actions/send_plain_hello.lua
@@ -1,25 +1,17 @@
-
-
-local event = {"request_received"}
-local priority = 1
+event: ["request_received"]
+priority: 1
 
 print("Hi")
-
-local function action (req)  
-  print("send_plain_hello handler")
-  response = {
-    headers = {
-      ["content-type"] = "text/plain",
-    },
-    body = "hello"
-  }
   
-  print("response")
-  return response
+print("send_plain_hello handler")
+response = {
+  headers = {
+    ["content-type"] = "text/plain",
+  },
+  body = "hello"
+}
+
+print("response")
+return response
 end
 
-return {
-    event = event,
-    action = action,
-    priority = priority
-}
--- rules/respond_to_any_request.lua
+++ rules/respond_to_any_request.lua
@@ -1,10 +1,7 @@
-function rule(req, events)
-    print("rule respond_to_any_request")
-    --if req.method == "GET" and req.path:match("*") then
-        print("respond_to_any_request triggered the event")
-        events["request_received"]:trigger(req)
-    --end
+
+print("rule respond_to_any_request")
+--if req.method == "GET" and req.path:match("*") then
+    print("respond_to_any_request triggered the event")
+    events["request_received"]:trigger(req)
+--end
 end
-return {
-    rule = rule
-}
