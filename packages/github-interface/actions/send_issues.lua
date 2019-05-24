ef5ee6ffbe0b1a8541475f12e39044b0ac9bd923
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2019-05-24 16:04:26 -0500

    Tag parsing, html rendering

--- packages/github-interface/actions/send_issues.lua
+++ packages/github-interface/actions/send_issues.lua
@@ -6,16 +6,25 @@ local url = "https://api.github.com/search/issues?q={%22lighttouch%22}"
 
 local github_response = send_request(url)
 
+local issues = github_response.body.items
+for _, issue in ipairs(issues) do
+  issue.tags = {}
+  for _, label in ipairs(issue.labels) do
+    local name, value = label.name:match("^(.+)/(.+)$")
+    if name then
+      issue.tags[name] = value
+    end
+  end
+end
+
 print("Status: ", github_response.status)
 print("Content length: ", #github_response.body_raw)
 
 response = {
   headers = {
-    --["content-type"] = "text/html",
-    ["content-type"] = "application/json",
+    ["content-type"] = "text/html",
   },
-  --body = render("issues.html")
-  body = github_response.body_raw
+  body = render("issues.html", {issues = issues})
 }
 
 return response
