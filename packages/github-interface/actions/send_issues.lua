44954978a082ba8a35a4554a484415d275e9662e
Luis Reyes <luisrogelio98@gmail.com>
2019-07-18 16:11:12 -0400

    multiple filters WIP

--- packages/github-interface/actions/send_issues.lua
+++ packages/github-interface/actions/send_issues.lua
@@ -2,14 +2,6 @@ event = ["issues_request_received"]
 priority = 1
 input_parameters = ["request"]
 
-local filters
--- to avoid null pointer if the value is not present it will be an empty string
-if request.query.filters then
-  filters = request.query.filters
-else
-  filters = ""
-end
-
 --Method to add extra parameters to a github request
 function addParameterToURL( url,name,value)
   local url_c = url
@@ -26,11 +18,11 @@ end
 
 -- Method to add client secret and client id for request authentication of the github api
 function addAuth( param_url )
-  --adding cleint id to the request url
+  --adding client id to the request url
   if settings.github_client_id then
     param_url = addParameterToURL(param_url,"client_id",settings.github_client_id)
   end
-  --adding client secrete to the request url
+  --adding client secret to the request url
   if settings.github_client_secret then
     param_url = addParameterToURL(param_url,"client_secret",settings.github_client_secret)
   end
@@ -46,32 +38,71 @@ function split(s, delimiter)--split a string
   return result;
 end
 
---currently only filtering labels
-function addFilters( _filters )
-  if _filters == "" then
-    return _filters
+-- stops a string to have repeated patterns
+-- to avoid having multiple of the same filter
+function sanitizeString(main_s, added_s )
+  if string.find( main_s, added_s, 1) then
+    -- the added_s already belongs to the main_s
+    log.debug("main" .. main_s)
+    return main_s
   end
+  log.debug("concatenated" .. main_s .. added_s)
+
+  return main_s .. added_s
+end
+
+--currently only filtering labels
+function addFilters( _filters , previous_filters)
+  -- if filters is nil and previous filters is not nil
+  if not _filters and previous_filters then
+    -- current filters is nil but previous ones are not
+    return previous_filters
+
+  elseif not previous_filters then
+    --without previous filters or current filters
+    return ""
+  end
+
   local result = ""
-  local labels = split(_filters,",")--spliting the filters in a table from filter text separting by commas ,
-  for _,field in pairs(labels) do
+  -- local filter_labels = split(_filters,",")
+
+  --spliting the filters in a table from filter text separting by commas ,
+  for _,field in pairs(split(_filters,",")) do
     local name, value = field:match("^(.+):(.+)$")
-    if name == "body" or name == "title" or name == "comments" then
-      --adding filter when it is body,title or comments
-      -- it will filter issues that have the written text whitin the field
-      result = result .. ' \"' .. value .. '\" in:' .. name
-    elseif name == "label" then
-      -- will filter for the specific written label
-      result = result .. ' ' .. name .. ':\"' .. value .. '\"'
-    else
-      -- if not any of the previous ones will assume it is a custom tag based of a label
-      -- the label will be replace the name/value
-      result = result .. ' label:\"' .. name .. "/".. value ..  '\"'
+
+    if value then --if the filter has the right format value will not be null
+
+      if name == "body" or name == "title" or name == "comments" then
+        --adding filter when it is body,title or comments
+        -- it will filter issues that have the written text whitin the field
+        result = sanitizeString(result,' \"' .. value .. '\"in:' .. name)
+
+      elseif name == "label" then
+        -- will filter for the specific written label
+        result = sanitizeString(result,' ' .. name .. ':\"' .. value .. '\"')
+
+      else
+        -- if not any of the previous ones will assume it is a custom tag based of a label
+        -- the label will be replace the name/value
+        result = sanitizeString(result , ' label:\"' .. name .. "/".. value ..  '\"')
+
+      end--end if types of filters
+
+    end --end if value not null
+
+  end -- end for adding filters
+
+  if previous_filters then --add the previous filters if there are any
+    for _,filter in pairs(split(previous_filters," ")) do
+      result = sanitizeString(result,filter)
     end
   end
+
   return result
 end
 
-local url = "https://api.github.com/search/issues?q={ type:issue ".. addFilters(filters) .." lighttouch }" --adding query filters in the url before making the requests
+local currentfilters = addFilters(request.query.filters,request.query.previous_filters)
+local url = "https://api.github.com/search/issues?q={ type:issue ".. currentfilters .." lighttouch }" --adding query filters in the url before making the requests
 
 url = addAuth(url) --adding github app auth if it was added in the lighttouch.scl
 
@@ -135,7 +166,8 @@ response = {
   body = render("issues.html", {
     issues = issues,
     all_tags = all_tags,
-    query =  json.from_table(request.query)
+    query =  json.from_table(request.query),
+    previous_filters = currentfilters,
   })
 }
 
