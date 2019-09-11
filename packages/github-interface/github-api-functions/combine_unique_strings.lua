107b17ff910a9ce30d844e30a0c3f782dd68ba2c
Luis Reyes <luisrogelio98@gmail.com>
2019-09-10 23:45:48 -0400

    removed deprecated functions

--- packages/github-interface/github-api-functions/combine_unique_strings.lua
+++
@@ -1,8 +0,0 @@
-function github_api.combine_unique_strings(main_s, added_s )
-    -- check to add a unique string into another string
-    if string.find( main_s, added_s, 1) then
-        -- the added_s already belongs to the main_s
-        return main_s
-    end
-    return main_s .. added_s
-end
