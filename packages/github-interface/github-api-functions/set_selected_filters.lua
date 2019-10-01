1d648cde05690d7cf4fdc7c178a013df3038e09c
Luis Reyes <luisrogelio98@gmail.com>
2019-10-01 12:34:57 -0400

    github filter issues fix

--- packages/github-interface/github-api-functions/set_selected_filters.lua
+++ packages/github-interface/github-api-functions/set_selected_filters.lua
@@ -2,7 +2,7 @@ function github_api.set_selected_filters(query, issues_tags)
     -- Sets a centinel value to show the selected values
     -- in checkboxes of the frontend
 
-    for k,v in pairs(query) do -- for each query parameter
+    for k,v in pairs(query.query) do -- for each query parameter
         if string.find(k,"selection") then -- if the parameter has the word selection
             local name, value = v:match("^(.+):(.+)$") -- name and value of the selection
             -- especial check for empty values
@@ -21,7 +21,9 @@ function github_api.set_selected_filters(query, issues_tags)
                 issues_tags[name]['values'][value]['value'] = value
             end
             -- end special check for empty values
+
             --average check for values
+            -- if the value exists it is already selected
             if issues_tags[name]['values'][value]['value'] then
                 issues_tags[name]['has_checked'] = true
                 issues_tags[name]['values'][value]['is_checked'] = true
